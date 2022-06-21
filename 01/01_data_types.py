7720421863aead0feffbf39e68261ea5fe0cbe8a
Shubham Dalvi <shubham.dalvi008@gmail.com>
2022-06-21 13:19:18 +0530

    DAY1 part3

---
+++ 01/01_data_types.py
@@ -0,0 +1,30 @@
+########### integer
+a = 10
+
+########### float
+b = 110.0
+
+########### boolean
+c = True
+
+########### string
+d = "Name"
+
+########### complex
+e = 8 + 5j
+
+########### list
+list = [10, "Name", True]
+
+########### Tuple
+tuple = (10, "Name", True)
+
+########### Dictionary
+dict = {
+    "name": "Shubham",
+    "age": 21,
+    "edu": ["10th", "HSC", 15]
+    }
+
+########### Set
+set = {10, "Name", 1.2j}
